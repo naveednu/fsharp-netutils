3fd256af390038357b50f323113c31668043f82a
naveednu <naveednu>
2013-06-22 13:50:55 +0500

    use local dns servers add NS support

--- nslookup.fsx
+++ nslookup.fsx
@@ -1,16 +1,24 @@
 open System.Net
 open System.Net.Sockets
+open System.Net.NetworkInformation
 open System.Text
 
+let getLocalDns =
+    let res = NetworkInterface.GetAllNetworkInterfaces()
+              |> Seq.map (fun (ifs) -> 
+                           Set.ofSeq ((ifs.GetIPProperties().DnsAddresses) 
+                           |> Seq.map (fun x -> x.ToString())))
+                        
+    let addresses = res |> Set.unionMany
+    addresses |> Seq.head
 
-let dns = "8.8.8.8"
 let Bytes2Int16 (x:byte[]) : int16 =  (int16 x.[0]<<<8||| int16 x.[1])
 
 let rec parseQName (acc) (data:byte[]) (start:int) =
     let pos = start
     let len = int data.[pos]
     if len > 0 then
-        if len = 192 then
+        if len = 0xC0 then
             let result, _ = parseQName (acc) (data) (int data.[pos+1])
             result, (start+2)
         else
@@ -21,6 +29,7 @@ let rec parseQName (acc) (data:byte[]) (start:int) =
     
 type QueryType =
     | A = 1s
+    | NS = 2s
     | CNAME = 5s
     | MX = 15s
     
@@ -42,19 +51,25 @@ type Answer(data:byte[], start:int) =
     member x.print()=
         printfn "answer name = %s, type = %d, class = %d rlength =%d, rdstart = %d" (x.QName) x.Type x.Class x.RDLength x.RDStart
     member x.printRecord()=
-        if x.Type = (int16)QueryType.MX then
+        let qtype = QueryType.ToObject(typeof<QueryType>, x.Type) :?> QueryType
+        match qtype with
+        | QueryType.MX -> 
             let exchange, _ = parseQName "" data (x.RDStart+2)
             printfn "%s\tmail exchanger = %d %s" x.QName (Bytes2Int16 data.[x.RDStart..x.RDStart+2]) exchange
-        else if x.Type = (int16) QueryType.CNAME then
+        | QueryType.NS ->
+            let nameServer, _ = parseQName "" data (x.RDStart)
+            printfn "%s\tnameserver = %s" x.QName nameServer
+        | QueryType.CNAME ->
             let cname, _ = parseQName "" data (x.RDStart)
             printfn "%s    canonical name = %s" x.QName cname
-        else if x.Type = (int16) QueryType.A then
+        | QueryType.A ->
             printfn "Name:   %s" x.QName
             printf "Address: "
             Array.iteri (fun i octet -> 
                           match i with
                           | 3 -> printf "%d\n" (int(octet))
                           | _ -> printf "%d." (int(octet))) data.[x.RDStart..x.RDStart+3]
+        | _ -> printfn "Invalid type"
                           
 type ResponsePacket(data:byte[]) = 
 
@@ -84,14 +99,19 @@ type ResponsePacket(data:byte[]) =
                 acc
         _gans [] _ansPosition (int x.NumAnswers)
 
-let nslookup(domainName: string, queryType: QueryType) =   
-    let udpClient = new UdpClient(dns, 53)
-    
+let buildQuery (domain:string) (queryType:QueryType) =
     let header = [ 14uy; 79uy; 1uy; 0uy; 0uy; 1uy; 0uy; 0uy; 0uy; 0uy; 0uy; 0uy ]
-    let parts = domainName.Split('.')
+    let parts = domain.Split('.')
     let domainParts = Array.fold (fun acc (x:string) -> acc @ [byte(x.Length)] @ [for y in x -> byte(y)]) [] parts
     let query = header @ domainParts @ [0uy; 0uy; byte(queryType); 0uy; 1uy;]
-    udpClient.Send(List.toArray query, query.Length) |> ignore
+    query |> List.toArray
+
+let nslookup(domainName: string, queryType: QueryType) = 
+    let dnsServer = getLocalDns  
+    let udpClient = new UdpClient(dnsServer, 53)
+    let query = buildQuery domainName queryType
+    
+    udpClient.Send(query, query.Length) |> ignore
     let ipe = new IPEndPoint(IPAddress.Any, 0)
     let result = udpClient.Receive(ref ipe)
       
@@ -111,6 +131,7 @@ let nslookup(domainName: string, queryType: QueryType) =
 if fsi.CommandLineArgs.Length < 3 then
     printfn "Usage: %s <domain> <query>" fsi.CommandLineArgs.[0]
     exit(0)
+    
 let domain = fsi.CommandLineArgs.[1]
 let query = fsi.CommandLineArgs.[2]
 
@@ -118,5 +139,6 @@ match query.ToLower() with
 | "mx" -> nslookup(domain, QueryType.MX)
 | "a" -> nslookup(domain, QueryType.A)
 | "cname" -> nslookup(domain, QueryType.CNAME)
+| "ns" -> nslookup(domain, QueryType.NS)
 | _ -> printfn "Unknwon query specificed"
 
